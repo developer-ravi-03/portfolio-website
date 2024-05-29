ffae60a25be59bb37d2975e6d8729206fa7c6e49
Ravi Kumar <ravibseb96@gmail.com>
2024-05-29 22:32:31 +0530

    some thing added

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -46,7 +46,7 @@ const EmailSection = () => {
         <section className='grid md:grid-cols-2 my-6 md:my-6 py-12 gap-4' id='contact'>
             {/* <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-purple-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-1/2 -left-4 transform -translate-x-1/2 -translate-1/2"></div> */}
             <div>
-                <h5 className='text-xl font-bold text-white my-2 '>Let's Connect</h5>
+                <h5 className='text-xl font-bold text-white my-2 '>Let&apos;s Connect</h5>
                 <p className='text-[#ABD7BE] mb-4 max-w-md'>
                     {" "}
                     I&apos;m currently looking for new opportunities, my inbox is always
