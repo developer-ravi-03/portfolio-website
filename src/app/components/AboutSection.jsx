1b8598b8808e6cca81efffd5136d152f8116e9c1
Ravi Kumar <ravibseb96@gmail.com>
2024-11-03 18:53:26 +0530

    skill updated

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -12,6 +12,7 @@ const TAB_DATA = [
         <li>Node.js</li>
         <li>Express</li>
         <li>React.js</li>
+        <li>Redux</li>
         <li>Next.js</li>
         <li>PHP</li>
         <li>MySQL</li>
